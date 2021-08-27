ae8f08eab0faf9f92ec029a930b04d5ce1d35b28
Jesse Tran <jessetran03@gmail.com>
2021-08-27 00:10:02 -0500

    updated production error

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
 app.use(function errorHandler(error, req, res, next) {
   let response
   if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
+    response = { error: { message: error.message, error } }
   } else {
     console.error(error)
     response = { message: error.message, error }
