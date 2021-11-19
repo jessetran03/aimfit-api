831508de035d4445baa307a60a3438ae6c7b8390
Jesse Tran <jessetran03@gmail.com>
2021-11-18 23:14:12 -0600

    prod error

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ const { PORT, DATABASE_URL, NODE_ENV } = require("./config");
 
 const db = knex({
   client: "pg",
-  connection: DATABASE_URL,
-  ssl: NODE_ENV === "production" ? true : false,
+  connection: {
+    connectionString: DATABASE_URL,
+    ssl: NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  },
 });
 
 app.set("db", db);
